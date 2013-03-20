db0cadeec289d88640642a26444e8f0eb28f69fe
Tomas Peterka <prestizni@gmail.com>
2013-03-20 12:08:29 +0100

    django-invoice is installable

--- setup.py
+++ setup.py
@@ -4,30 +4,22 @@ APP_NAME = "django-invoice"
 VERSION = 0.3
 
 setup(
-    author='Tomas Peterka',
-    author_email='prestizni@gmail.com',
     name=APP_NAME,
     version=VERSION,
     description='Pluggable django invoicing app',
+    packages=[
+        'invoice',
+        'invoice.utils'
+    ],
+
+    author='Tomas Peterka',
+    author_email='prestizni@gmail.com',
+    licence="GPL v3",
     url='http://pypi.python.org/pypi/{0}/'.format(APP_NAME),
+    keywords="django invoice pdf",
+
     install_requires=[
         "django>=1.3",
         "reportlab",
     ],
-    modules=[
-        'invoice',
-        'invoice.management',
-        'invoice.management.commands',
-        'invoice.utils',
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Utilities'
-    ]
 )
