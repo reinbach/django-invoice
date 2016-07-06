3e4370cc27d0981cb66d61af6a37e2797254be1c
Tomas Peterka <prestizni@gmail.com>
2016-07-06 22:57:03 +0200

    Update suncdb to migration

--- invoice/test_data/__init__.py
+++ invoice/test_data/__init__.py
@@ -5,41 +5,43 @@ from invoice.models import Invoice, InvoiceItem, Address, BankAccount
 
 
 def load():
-    contractor, created = (Address.objects
-                                  .get_or_create(name='Simon Luijk',
-                                                 street=u'Rydsvägen 242',
-                                                 town=u'Linköping',
-                                                 defaults=dict(
-                                                     postcode='584 64',
-                                                     country=u"Sweden",
-                                                     business_id="523489473",
-                                                     tax_id="US092748793",
-                                                     extra=('Phone: 076-577-9284\n'
-                                                            'Email: boss@example.com')
-                                                 )))
+    contractor, created = Address.objects.get_or_create(
+        name='Simon Luijk',
+        street=u'Rydsvägen 242',
+        town=u'Linköping',
+        defaults=dict(
+            postcode='584 64',
+            country=u"Sweden",
+            business_id="523489473",
+            tax_id="US092748793",
+            extra=('Phone: 076-577-9284\n'
+                    'Email: boss@example.com')
+    )))
 
-    subscriber, created = (Address.objects
-                                  .get_or_create(name=u'Tomáš Peterka',
-                                                 street=u'Zdislavická',
-                                                 town=u'Praha',
-                                                 defaults=dict(
-                                                     postcode='142 00',
-                                                     country=u"Czech Republic",
-                                                     extra="Email: atheiste@seznam.cz"
-                                                 )
-                                                 )
-                           )
-    account, created = BankAccount.objects.get_or_create(number=782634210, bank=6250)
+    subscriber, created = (Address.objects.get_or_create(
+        name=u'Tomáš Peterka',
+        street=u'Zdislavická',
+        town=u'Praha',
+        defaults=dict(
+            postcode='142 00',
+            country=u"Czech Republic",
+            extra="Email: atheiste@seznam.cz"
+    )))
+
+    account, created = BankAccount.objects.get_or_create(
+        number=782634210, bank=6250)
 
     basedir = os.path.dirname(os.path.abspath(__file__))
     logo = os.path.join(basedir, "logo.png")
     if not os.path.exists(logo):
         logo = None
 
-    invoice, c = Invoice.objects.get_or_create(contractor=contractor,
-                                               subscriber=subscriber,
-                                               defaults=dict(logo=logo,
-                                                             contractor_bank=account))
+    invoice, c = Invoice.objects.get_or_create(
+        contractor=contractor,
+        subscriber=subscriber,
+        defaults=dict(logo=logo,
+                      contractor_bank=account))
+
     InvoiceItem.objects.get_or_create(
         invoice=invoice, description="Bunch of cow-horse meat",
         defaults={"quantity": 10, "unit_price": Decimal("550.00")})
--- testapp/testapp/management.py
+++ testapp/testapp/management.py
@@ -1,10 +1,11 @@
-from django.db.models.signals import post_syncdb
+from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
 from invoice import models
 from invoice import test_data
 
 
-@receiver(post_syncdb, sender=models)
+@receiver(post_migrate, sender=models)
 def load_test_data(sender, **kwargs):
+    """Load test model-data and images."""
     test_data.load()
